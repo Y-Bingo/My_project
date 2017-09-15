de595e07bf2586b005593f74fa30d9848327197d
Y-Bingo <Y_Bingo@163.com>
2017-09-16 00:55:21 +0800

    new 小程序

---
+++ reader/pages/post/index.js
@@ -0,0 +1,26 @@
+var localDb = require('../../data/localDb');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  },
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+    // this.data.postList = localDb.postList;
+    this.setData({
+        postList: localDb.postList
+    });
+  },
+  postDetail:function(event){
+      wx.navigateTo({
+          url: './post-detail/post-detail'
+      })
+  }
+})
